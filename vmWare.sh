389c8df5691cf2f9ecdaf0cf7801f4a71ee42f52
httpEugene <eugen.kiselov@gmail.com>
2017-01-09 21:08:39 +0200

    Update vmWare.sh

--- vmWare.sh
+++ vmWare.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo curl https://download.owncloud.org/download/repositories/stable/Ubuntu_16.04/Release.key | sudo apt-key add -
+echo 'deb https://download.owncloud.org/download/repositories/stable/Ubuntu_16.04/ /' | sudo tee /etc/apt/sources.list.d/owncloud.list
