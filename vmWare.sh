0285a783b6fe6109494b6ccc8a2bf054027deb5e
httpEugene <eugen.kiselov@gmail.com>
2017-01-09 22:06:40 +0200

    Update vmWare.sh

--- vmWare.sh
+++ vmWare.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo apt-get update
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt
